6a1dc40395db228b9b3d871d427abff1ab7cad66
ali207rezaee-ctrl <ali.207.rezaee@gmail.com>
2026-06-12 22:42:25 +0330

    app.js

--- app.js
+++ app.js
@@ -1,47 +1,20 @@
-const user_id = Telegram.WebApp.initDataUnsafe.user.id;
-
-async function loadMessages() {
-  const res = await fetch("/messages");
-  const data = await res.json();
-
-  const box = document.getElementById("chat-box");
-  box.innerHTML = "";
-
-  data.forEach(m => {
-    const div = document.createElement("div");
-    div.classList.add("msg");
-
-    if (m.role === "user") {
-      div.classList.add("user");
-      div.innerText = "👤: " + m.text;
-    } else {
-      div.classList.add("admin");
-      div.innerText = "👨🏻‍💻: " + m.text;
-    }
-
-    box.appendChild(div);
-  });
-
-  box.scrollTop = box.scrollHeight;
-}
-
 async function sendMessage() {
-  const input = document.getElementById("message");
+    let input = document.getElementById("msg");
 
-  if (!input.value) return;
+    let message = input.value;
 
-  await fetch("/send", {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({
-      text: input.value,
-      user_id: user_id
-    })
-  });
+    let user = Telegram.WebApp.initDataUnsafe.user;
 
-  input.value = "";
-  loadMessages();
+    await fetch("https://YOUR-SERVER-URL/send", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            message: message,
+            user_id: user.id
+        })
+    });
+
+    input.value = "";
 }
-
-setInterval(loadMessages, 2000);
-loadMessages();
